8c18da20766fd5da6816bf5187896fd1d4cb0996
HASANALI117 <hassanali61911@gmail.com>
2025-06-11 04:07:01 +0300

    Adds session management and authentication support
    Introduces session handling using `express-session` and authentication with Passport.js to enable secure user management. Refactors server setup by extracting app initialization into a utility function for better modularity. Updates dependencies to include necessary libraries and their types for session and authentication functionality.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,7 @@
-import express from "express";
-import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
 import { config } from "dotenv";
-import cors from "cors";
+import { createApp } from "./utils/createApp";
 config({ path: "../.env" });
-
-// Routes
-import routes from "./routes/";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 8000;
@@ -20,12 +15,7 @@ async function main() {
       .catch((err) => console.error("Failed to connect to MongoDB", err));
 
     // Express server setup
-    const app = express();
-    app.use(express.json());
-    app.use(cors({ origin: "http://localhost:3000", credentials: true }));
-    app.use(cookieParser());
-
-    app.use("/api", routes);
+    const app = createApp();
 
     app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
---
+++ backend/src/utils/createApp.ts
@@ -0,0 +1,38 @@
+import express, { Express } from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import session from "express-session";
+import passport from "passport";
+import routes from "../routes";
+
+export const createApp = (): Express => {
+  const app = express();
+
+  //   Enable parser for JSON requests
+  app.use(express.json());
+  //   Enable CORS
+  app.use(cors({ origin: ["http://localhost:3000"], credentials: true }));
+  //   Enable cookie parsing
+  app.use(cookieParser());
+  //   Enable session management
+  app.use(
+    session({
+      secret: `${process.env.SESSION_SECRET}`,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        //   secure: process.env.NODE_ENV === "production", // Use secure cookies in production
+        //   httpOnly: true, // Prevent client-side JavaScript from accessing the cookie
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 1 day
+      },
+    })
+  );
+
+  //   Initialize Passport.js for authentication
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  app.use("/api", routes);
+
+  return app;
+};
