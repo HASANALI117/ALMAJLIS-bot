b071cb05e9868c3a9468def6d55c0e1fef4dae31
HASANALI117 <hassanali61911@gmail.com>
2025-07-18 10:40:01 +0300

    Renames WelcomeChannel to WelcomeSettings and updates schema
    Updates the database model to reflect a broader scope by renaming WelcomeChannel to WelcomeSettings.
    
    Adds a new `enabled` field to the schema for toggling welcome messages and adjusts related imports and references accordingly.

--- bot/events/guildMemberAdd/send-welcome-message.js
+++ bot/events/guildMemberAdd/send-welcome-message.js
@@ -1,4 +1,4 @@
-import WelcomeChannel from "../../models/WelcomeChannel.js";
+import WelcomeSettings from "../../models/WelcomeSettings.js";
 import { EmbedBuilder } from "discord.js";
 
 export default async (member) => {
@@ -8,7 +8,7 @@ export default async (member) => {
     );
 
     // Fetch welcome settings from database
-    const welcomeSettings = await WelcomeChannel.findOne({
+    const welcomeSettings = await WelcomeSettings.findOne({
       guildId: member.guild.id,
     });
 
